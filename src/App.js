2b367debb1c6ca2bab994c707b0ecdde6a7f3c0c
Hitesh Moudgil <officialmoudgil@gmail.com>
2022-11-30 22:55:48 -0500

    winning functionality working for both players

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Box from "./components/Box.js";
 
@@ -7,15 +7,65 @@ function App() {
   let player2 = "O";
 
   const [board, setBoard] = useState([]);
+  const [gameFinished, setGameFinished] = useState(false);
   // console.log(board);
   const [currentPlayer, setCurrentPlayer] = useState(player1);
+  const [lastPlayerMoved, setLastPlayerMoved] = useState(player1);
+
+  // console.log(board);
+  const determineWinner = () => {
+    const filteredPlayerArray = board.filter(
+      (x) => x.player === lastPlayerMoved
+    );
+    // const filteredPlayerArray2 = board.filter((x) => x.player === player2);
+    console.log(filteredPlayerArray);
+    // console.log(filteredPlayerArray2);
+    const filteredPlayerPositionArray = filteredPlayerArray.map(
+      (x) => x.position
+    );
+    if (
+      (filteredPlayerPositionArray.includes("box1") &&
+        filteredPlayerPositionArray.includes("box2") &&
+        filteredPlayerPositionArray.includes("box3")) ||
+      (filteredPlayerPositionArray.includes("box4") &&
+        filteredPlayerPositionArray.includes("box5") &&
+        filteredPlayerPositionArray.includes("box6")) ||
+      (filteredPlayerPositionArray.includes("box7") &&
+        filteredPlayerPositionArray.includes("box8") &&
+        filteredPlayerPositionArray.includes("box9")) ||
+      (filteredPlayerPositionArray.includes("box1") &&
+        filteredPlayerPositionArray.includes("box5") &&
+        filteredPlayerPositionArray.includes("box9")) ||
+      (filteredPlayerPositionArray.includes("box1") &&
+        filteredPlayerPositionArray.includes("box4") &&
+        filteredPlayerPositionArray.includes("box7")) ||
+      (filteredPlayerPositionArray.includes("box2") &&
+        filteredPlayerPositionArray.includes("box5") &&
+        filteredPlayerPositionArray.includes("box8")) ||
+      (filteredPlayerPositionArray.includes("box3") &&
+        filteredPlayerPositionArray.includes("box6") &&
+        filteredPlayerPositionArray.includes("box9")) ||
+      (filteredPlayerPositionArray.includes("box3") &&
+        filteredPlayerPositionArray.includes("box5") &&
+        filteredPlayerPositionArray.includes("box7"))
+    ) {
+      console.log(lastPlayerMoved + " wins");
+      setGameFinished(true);
+    }
+  };
 
   const clickHandler = (event) => {
-    if (!board.find((x) => x.position === event.target.id)) {
+    const isCurrentPositionOccupied = board.find(
+      (x) => x.position === event.target.id
+    );
+
+    if (!isCurrentPositionOccupied && !gameFinished) {
       setBoard((prevArrayValue) => [
         ...prevArrayValue,
         { position: event.target.id, player: currentPlayer },
       ]);
+
+      setLastPlayerMoved(currentPlayer);
 
       if (currentPlayer === player1) {
         setCurrentPlayer(player2);
@@ -25,43 +75,17 @@ function App() {
     }
   };
 
-  const determineWinner = () => {
-    const filteredPlayerArray = board.filter((x) => x.player === player1);
-    // console.log(filteredPlayerArray);
-    const filteredPlayerPositionArray = filteredPlayerArray.map(
-      (x) => x.position
-    );
-    if (
-      filteredPlayerPositionArray.includes("box1") &&
-      filteredPlayerPositionArray.includes("box2") &&
-      filteredPlayerPositionArray.includes("box3") ||
-      filteredPlayerPositionArray.includes("box4") &&
-      filteredPlayerPositionArray.includes("box5") &&
-      filteredPlayerPositionArray.includes("box6") ||
-      filteredPlayerPositionArray.includes("box7") &&
-      filteredPlayerPositionArray.includes("box8") &&
-      filteredPlayerPositionArray.includes("box9") ||
-      filteredPlayerPositionArray.includes("box1") &&
-      filteredPlayerPositionArray.includes("box5") &&
-      filteredPlayerPositionArray.includes("box9") ||
-      filteredPlayerPositionArray.includes("box1") &&
-      filteredPlayerPositionArray.includes("box4") &&
-      filteredPlayerPositionArray.includes("box7") ||
-      filteredPlayerPositionArray.includes("box2") &&
-      filteredPlayerPositionArray.includes("box5") &&
-      filteredPlayerPositionArray.includes("box8") ||
-      filteredPlayerPositionArray.includes("box3") &&
-      filteredPlayerPositionArray.includes("box6") &&
-      filteredPlayerPositionArray.includes("box9") ||
-      filteredPlayerPositionArray.includes("box3") &&
-      filteredPlayerPositionArray.includes("box5") &&
-      filteredPlayerPositionArray.includes("box7")
-    )
-      console.log('X wins');
-  };
+  // const buttonHandler = () => {
 
-  determineWinner();
-  // console.log(board.find((x) => x.position === "box1"));
+  // };
+
+
+  useEffect(() => {
+    determineWinner();
+    return () => {
+      // Clean up the subscription
+    };
+  });
 
   return (
     <div>
@@ -83,6 +107,7 @@ function App() {
         <Box clickHandler={clickHandler} board={board} id="box8" />
         <Box clickHandler={clickHandler} board={board} id="box9" />
       </div>
+      {/* <button onClick={buttonHandler}>Reset</button> */}
     </div>
   );
 }
